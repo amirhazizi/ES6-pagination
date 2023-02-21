ab9f86cf48eec793cd1fc0471defb01d39bff7a9
amir <amirazizihosein@gmail.com>
2023-02-21 16:42:09 +0330

    display Followers

--- js/app.js
+++ js/app.js
@@ -3,8 +3,11 @@ import displayFollowers from "./displayFollowers.js"
 import paginate from "./paginate.js"
 import displayButtons from "./displayButtons.js"
 
+const title = document.querySelector(".section-title h1")
 // console.log(await fetchFollowers())
-window.addEventListener("load", init)
 const init = async () => {
   const followers = await fetchFollowers()
+  displayFollowers(followers)
+  title.textContent = "Pagination"
 }
+window.addEventListener("load", init)
