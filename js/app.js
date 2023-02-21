10e33d29ebe4daa6330d60a3c416fbb0363801ad
amir <amirazizihosein@gmail.com>
2023-02-21 22:33:33 +0330

    finish btn functionality

--- js/app.js
+++ js/app.js
@@ -19,4 +19,19 @@ const init = async () => {
   page = paginate(followers)
   setupUI()
 }
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-container")) return
+  if (e.target.classList.contains("page-btn"))
+    index = parseInt(e.target.dataset.index)
+  if (e.target.classList.contains("next-btn")) {
+    index++
+    if (index > page.length - 1) index = 0
+  }
+  if (e.target.classList.contains("prev-btn")) {
+    index--
+    if (index < 0) index = page.length - 1
+  }
+
+  setupUI()
+})
 window.addEventListener("load", init)
--- js/paginate.js
+++ js/paginate.js
@@ -1,7 +1,7 @@
 const paginate = (followers) => {
   const itemPerPage = 10
   const numberOfPages = Math.ceil(followers.length / itemPerPage)
-  const newFollowers = Array.from({ length: itemPerPage }, (_, index) => {
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
     const start = index * itemPerPage
     return followers.slice(start, start + itemPerPage)
   })
